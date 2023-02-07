f5f5e76fc5911ef59cb179f31a656ab7abb8d8e0
Alexander Radchenko <radchenkoalexd@gmail.com>
2023-02-07 15:42:30 +0200

    Added PerfectPay behind logic

--- PerfectPay/MainPage.xaml.cs
+++ PerfectPay/MainPage.xaml.cs
@@ -1,6 +1,68 @@
 ﻿namespace PerfectPay;
 public partial class MainPage : ContentPage
 {
+	private decimal _bill;
+	private int _tip;
+	private int _personsNumber;
+
 	public MainPage()
-		=> InitializeComponent();
+	{
+		InitializeComponent();
+
+		_personsNumber = 1;
+	}
+
+	private void OnEntryInputCompleted(object sender, EventArgs e)
+	{
+		_bill = decimal.Parse(BillTextEntry.Text);
+		CalculateTotal();
+	}
+
+	private void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
+	{
+		_tip = (int)TipSlider.Value;
+		PercentageTipLabel.Text = $"Tip: {_tip}%";
+		CalculateTotal();
+	}
+
+	private void OnTipButtonClicked(object sender, EventArgs e)
+	{
+		if(sender is not Button button) return;
+
+		var percentage = int.Parse(button.Text.Replace("%", string.Empty));
+		TipSlider.Value = percentage;
+
+	}
+
+	private void OnMinusButtonClicked(object sender, EventArgs e)
+	{
+		if(_personsNumber < 2) return;
+
+		_personsNumber--;
+		SetPersonsNumber();
+	}
+
+	private void OnPlusButtonClicked(object sender, EventArgs e)
+	{
+		_personsNumber++;
+		SetPersonsNumber();
+	}
+
+	private void SetPersonsNumber()
+	{
+		PersonsNumberLabel.Text = _personsNumber.ToString();
+		CalculateTotal();
+	}
+
+	private void CalculateTotal()
+	{
+		var totalTip = _bill * _tip / 100;
+		var tipByPerson = totalTip / _personsNumber;
+		var subtotal = _bill / _personsNumber;
+		var totalByPerson = (_bill + totalTip) / _personsNumber;
+
+		FlatTipLabel.Text = $"{tipByPerson:C}";
+		SubtotalLabel.Text = $"{subtotal:C}";
+		TotalLabel.Text = $"{totalByPerson:C}";
+	}
 }
